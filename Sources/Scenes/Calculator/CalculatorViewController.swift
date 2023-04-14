7b0486fdc4ef6bccb2e5eedb36c7e30899dfbad5
Nelson PIRES DA SILVA <piresdasilva.n@gmail.com>
2023-04-14 03:59:01 +0200

    architecture done

--- Sources/Scenes/Calculator/CalculatorViewController.swift
+++ Sources/Scenes/Calculator/CalculatorViewController.swift
@@ -1,14 +1,7 @@
-//
-//  ViewController.swift
-//  SimpleCalc
-//
-//  Created by Vincent Saluzzo on 29/03/2019.
-//  Copyright © 2019 Vincent Saluzzo. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class CalculatorViewController: UIViewController {
+
     @IBOutlet weak var textView: UITextView!
     @IBOutlet var numberButtons: [UIButton]!
 
